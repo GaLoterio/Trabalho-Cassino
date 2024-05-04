edbed8a6a16672cfdc56af2f38348d8a5e80b8af
Gabriel Loterio <loteriogabriel05@gmail.com>
2024-05-03 21:13:09 -0300

    update

--- Cassino/Main/src/BlackJack.java
+++ Cassino/Main/src/BlackJack.java
@@ -3,28 +3,46 @@ import java.util.Random;
 
 public class BlackJack {
     public static void main(String[] args) {
-
+ Random random = new Random();
         String[] nipes = { "♠", "♥", "♦", "♣" };
         String[] faces = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
         String total = "";
         String total2 = "";
+        String total3="21";
         int contador = 2;
-
+        int maoJogador = 0;
+        String nipe;
+        String face;
         for (int i = 1; i <= contador; i++) {
-            String nipe = nipes[(int) (Math.random() * 4)];
-            String face = faces[(int) (Math.random() * faces.length)];
+            nipe = nipes[(int) (Math.random() * 4)];
+            face = faces[(int) (Math.random() * faces.length)];
             total = face + nipe;
             total2 = face;
+           
             if (i == 1) {
                 JOptionPane.showMessageDialog(null, "Sua primeira carta é " + total);
             } else {
                 JOptionPane.showMessageDialog(null, "Sua segunda carta é " + total);
             }
-
         }
+        
+        maoJogador = Integer.parseInt(total2);
+        while(maoJogador < 21){
+                String resposta = JOptionPane.showInputDialog(null, "Você deseja pedir mais uma carta?");
+                if(resposta.equalsIgnoreCase("sim")){
+                    nipe = nipes[(int) (Math.random() * 4)];
+                    face = faces[(int) (Math.random() * faces.length)];
+                    total = face + nipe;
+                    JOptionPane.showMessageDialog(null, "Sua nova carta é " + total);
+                    maoJogador += Integer.parseInt(total);
+                } else {
+                    break;
+                }
+                
+            }
 
-        Random random = new Random();
-        int maoJogador = Integer.parseInt(total2);
+       
+        
         int maoDistribuidor = random.nextInt(7) + 15;
         String mensagem;
 
