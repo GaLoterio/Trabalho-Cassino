ae7d0251920b6ac0df0330b12f8866d9d9af9f1d
Gustavo Guimarães Pardini <ggpardini@gmail.com>
2024-05-16 20:54:24 -0300

    Update BacBo
    Corrigindo bugs e adicionando estatisticas
    
    Co-Authored-By: Bruno Silva  <101366729+20Bruno@users.noreply.github.com>

--- Cassino/Main/src/BacBo.java
+++ Cassino/Main/src/BacBo.java
@@ -2,6 +2,8 @@ import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
+import javax.swing.JOptionPane;
+
 public class BacBo {
 
     public static void main(String[] args) {
@@ -9,22 +11,24 @@ public class BacBo {
         Scanner sc = new Scanner(System.in);
 
         User jogador1 = new User("gustavo");
-        jogador1.Depositar();
-
+        
+        //inicializando variaveis
         int dado1A;
         int dado2A;
         int dado1V;
         int dado2V;
         double aposta;
-        double ganho;
         int escolha;
         int loop = 1;
 
         System.out.println("*******************");
         System.out.println("Bem vindo ao Bac Bo!");
         System.out.println("*******************");
+        JOptionPane.showMessageDialog(null, "Bac Bo é um jogo de dados onde você pode apostar numa cor, ou no empate.\n Os dados serão rolados, e a soma dos dados da sua cor for maior que a soma dos dados da outra cor, você ganha.");
 
         while (loop == 1) {
+            jogador1.vezesJogadasBacBo ++;
+
             System.out.println("Vamos jogar! Deseja apostar no AZUL, VERMELHO ou no EMPATE?");
             System.out.println("1: Azul \n2: Vermelho \n3: Empate");
             escolha = sc.nextInt();
@@ -35,6 +39,8 @@ public class BacBo {
                 System.out.println("Valor de aposta inválido, insira um valor válido:");
                 aposta = sc.nextDouble();
             }
+
+            jogador1.saldo -= aposta;
 
             if (escolha == 1) {
                 System.out.println("Aposta final: R$" + aposta + " no azul!\n Agora vamos rolar os dados...");
@@ -58,30 +64,38 @@ public class BacBo {
 
             if (totalAzul > totalVermelho) {
                 vitoriaAzul = true;
-                System.out.println("Passou por aqui 1");
             } else if (totalAzul < totalVermelho) {
                 vitoriaVermelho = true;
-                System.out.println("Passou por aqui 2");
             } else {
                 empate = true;
-                System.out.println("Passou por aqui 3");
             }
 
             if (vitoriaAzul == true && escolha == 1) {
                 System.out.println("Parabens voce ganhou!");
+                jogador1.saldo += aposta * 2;
+                System.out.println("Você ganhou " + aposta * 2 + " e seu novo saldo é de: R$" + jogador1.saldo + "!");
+                jogador1.vitoriasBacBo +=1;
             } else if (vitoriaVermelho == true && escolha == 2) {
                 System.out.println("Parabens voce ganhou!");
+                jogador1.saldo += aposta * 2;
+                System.out.println("Você ganhou " + aposta * 2 + " e seu novo saldo é de: R$" + jogador1.saldo + "!");
+                jogador1.vitoriasBacBo +=1;
             } else if (empate == true && escolha == 3) {
                 System.out.println("Parabens voce ganhou!");
+                jogador1.saldo += aposta * 5;
+                System.out.println("Você ganhou " + aposta * 5 + " e seu novo saldo é de: R$" + jogador1.saldo + "!");
+                jogador1.vitoriasBacBo +=1;
             } else {
                 System.out.println("Infelizmente voce perdeu!");
+                System.out.println("Você perdeu R$" + aposta + "! Seu novo saldo é de R$" + jogador1.saldo + "!");
+                jogador1.derrotasBacBo +=1;
             }
 
             System.out.println("Dados azuis: " + totalAzul);
             System.out.println("Dados vermelhos: " + totalVermelho);
 
             System.out.println("Deseja jogar novamente?");
-            System.out.println("1: Sim\n 2: Nao");
+            System.out.println("1: Sim\n2: Nao");
             loop = sc.nextInt();
         }
 
